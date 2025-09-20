6fddffb28261a49cd243890ce2e3ceca7e33943b
asymptoto <asymptoto@asymptoto.de>
2025-09-20 18:42:50 +0200

    removed deprecated comment

--- create_snapshots.py
+++ create_snapshots.py
@@ -6,7 +6,6 @@ def run(containers: list[common.Container]) -> None:
     print("Starting snapshot creation")
     for container in containers:
         now = datetime.now()
-        # errors are not caught. an error here should lead to termination to prevent updates without snapshots
         if container.id in config.EXCLUDE_SNAPSHOTS:
             print(f"Skipping snapshot creation for CT {container.id} due to exclude list")
             continue
